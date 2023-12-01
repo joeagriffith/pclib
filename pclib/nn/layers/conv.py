c2817006fd912e90f704749a722ae3ecd8fead64
Joe Griffith <joeagriffith@gmail.com>
2023-12-01 12:24:07 +0000

    tweaks and verification run

--- pclib/nn/layers/conv.py
+++ pclib/nn/layers/conv.py
@@ -5,114 +5,10 @@ import torch.nn.functional as F
 from torch import Tensor
 from torch.nn.parameter import Parameter
 from typing import Optional
+from pclib.utils.functional import reTanh
 
 # Whittington & Bogacz 2017
 class Conv2d(nn.Module):
-    __constants__ = ['shape', 'prev_shape']
-    shape: int
-    prev_shape: Optional[int]
-
-    def __init__(self,
-                 shape: (int, int, int), # (channels, height, width)
-                 prev_channels: int = None,
-                 kernel_size: int = 3,
-                 stride: int = 1,
-                 padding = 'same',
-                 bias: bool = True,
-                 maxpool=1,
-                 actv_fn: callable = F.tanh,
-                 d_actv_fn: callable = None,
-                 gamma: float = 0.1,
-                 beta: float = 1.0,
-                 device=torch.device('cpu'),
-                 dtype=None
-                 ) -> None:
-        
-        # assert stride == 1, "Stride != 1 not yet supported."
-
-        factory_kwargs = {'device': device, 'dtype': dtype}
-        super(Conv2d, self).__init__()
-
-        self.shape = shape
-        # self.prev_shape = prev_shape
-        self.actv_fn = actv_fn
-        self.gamma = gamma
-        self.beta = beta
-        self.device = device
-
-
-        # Default derivative of activation function
-        if d_actv_fn is not None:
-            self.d_actv_fn: callable = d_actv_fn
-        elif actv_fn == F.relu:
-            self.d_actv_fn: callable = lambda x: torch.sign(torch.relu(x))
-        elif actv_fn == F.sigmoid:
-            self.d_actv_fn: callable = lambda x: torch.sigmoid(x) * (1 - torch.sigmoid(x))
-        elif actv_fn == F.tanh:
-            self.d_actv_fn: callable = lambda x: 1 - torch.tanh(x).square()
-        
-        # Initialise weights if not input layer
-        if prev_channels is not None:
-            self.conv_td = nn.Sequential(
-                nn.Conv2d(shape[0], prev_channels, kernel_size, padding=padding, stride=stride, bias=bias, **factory_kwargs),
-                nn.MaxPool2d(kernel_size=maxpool),
-            )
-            # self.conv_bu = nn.Sequential(
-            #     nn.Upsample(scale_factor=maxpool),
-            #     nn.ConvTranspose2d(prev_shape[0], shape[0], kernel_size, padding=padding, stride=stride, bias=False, **factory_kwargs),
-            # )
-
-    def init_state(self, batch_size):
-        return {
-            'x': torch.zeros((batch_size, self.shape[0], self.shape[1], self.shape[2]), device=self.device, requires_grad=True),
-            'e': torch.zeros((batch_size, self.shape[0], self.shape[1], self.shape[2]), device=self.device),
-        }
-
-    def to(self, *args, **kwargs):
-        self.device = args[0]
-        return super().to(*args, **kwargs)
-
-    # Returns a prediction of the state in the previous layer
-    def predict(self, state):
-        return self.conv_td(self.actv_fn(state['x']))
-    
-    # propagates error from layer below, return an update for x
-    def propagate(self, e_below):
-        # return self.conv_bu(e_below)
-        raise(NotImplementedError("propagate not implemented for Conv2d"))
-    
-    # Recalculates prediction-error between state and top-down prediction of it
-    # With simulated annealing
-    def update_e(self, state, pred=None, temp=None):
-        if pred is not None:
-            state['e'] = state['x'] - pred
-
-        if temp is not None:
-            eps = torch.randn_like(state['e'], device=self.device) * 0.034 * temp
-            state['e'] += eps
-
-    def update_x(self, state, e_below=None):
-        state['x'] = state['x'].detach() - self.gamma * state['x'].grad
-        state['x'].requires_grad = True
-
-    # def update_x(self, state, e_below=None):
-        # # If not input layer, propagate error from layer below
-        # if e_below is not None:
-        #     if e_below.dim() == 2:
-        #         e_below = e_below.unsqueeze(-1).unsqueeze(-1)
-        #     update = self.propagate(e_below)
-        #     state['x'] += self.gamma * (-state['e'] + update * self.d_actv_fn(state['x']))
-        # # This update will be zero if top layer
-        # state['x'] += self.gamma * (self.beta * -state['e'])
-        
-    # def update_grad(self, state, e_below):
-    #     if state['e'].norm() > 0:
-    #         state['e'].square().sum().backward()
-
-
-
-# Whittington & Bogacz 2017
-class ConvTranspose2d(nn.Module):
     __constants__ = ['shape', 'prev_shape']
     shape: int
     prev_shape: Optional[int]
@@ -134,17 +30,24 @@ class ConvTranspose2d(nn.Module):
                  dtype=None
                  ) -> None:
         
-        assert stride == 1, "Stride != 1 not yet supported."
+        # assert stride == 1, "Stride != 1 not yet supported."
 
-        factory_kwargs = {'device': device, 'dtype': dtype}
-        super(ConvTranspose2d, self).__init__()
+        self.factory_kwargs = {'device': device, 'dtype': dtype}
+        super(Conv2d, self).__init__()
 
         self.shape = shape
-        # self.prev_shape = prev_shape
         self.actv_fn = actv_fn
         self.gamma = gamma
         self.beta = beta
         self.device = device
+
+        self.prev_channels = prev_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.bias = bias
+        self.upsample = upsample
+        self.maxpool = maxpool
 
 
         # Default derivative of activation function
@@ -152,17 +55,24 @@ class ConvTranspose2d(nn.Module):
             self.d_actv_fn: callable = d_actv_fn
         elif actv_fn == F.relu:
             self.d_actv_fn: callable = lambda x: torch.sign(torch.relu(x))
+        elif actv_fn == F.leaky_relu:
+            self.d_actv_fn: callable = lambda x: torch.sign(torch.relu(x)) + torch.sign(torch.minimum(x, torch.zeros_like(x))) * 0.01
+        elif actv_fn == reTanh:
+            self.d_actv_fn: callable = lambda x: torch.sign(torch.relu(x)) * (1 - torch.tanh(x).square())
         elif actv_fn == F.sigmoid:
             self.d_actv_fn: callable = lambda x: torch.sigmoid(x) * (1 - torch.sigmoid(x))
         elif actv_fn == F.tanh:
             self.d_actv_fn: callable = lambda x: 1 - torch.tanh(x).square()
         
+        self.init_weights()
+        
+    def init_weights(self):
         # Initialise weights if not input layer
-        if prev_channels is not None:
+        if self.prev_channels is not None:
             self.conv_td = nn.Sequential(
-                nn.MaxPool2d(kernel_size=maxpool),
-                nn.Upsample(scale_factor=upsample),
-                nn.ConvTranspose2d(shape[0], prev_channels, kernel_size, stride=stride, padding=padding, bias=bias, **factory_kwargs),
+                nn.Conv2d(self.shape[0], self.prev_channels, self.kernel_size, padding=self.padding, stride=self.stride, bias=self.bias, **self.factory_kwargs),
+                nn.Upsample(scale_factor=self.upsample),
+                nn.MaxPool2d(kernel_size=self.maxpool),
             )
             # self.conv_bu = nn.Sequential(
             #     nn.Upsample(scale_factor=maxpool),
@@ -217,11 +127,53 @@ class ConvTranspose2d(nn.Module):
     #         state['e'].square().sum().backward()
 
 
+
+
+
+
 # Whittington & Bogacz 2017
-class Conv2dSkip(nn.Module):
-    __constants__ = ['shape', 'prev_shape']
-    shape: int
-    prev_shape: Optional[int]
+class ConvTranspose2d(Conv2d):
+
+    def __init__(self,
+                 shape: (int, int, int), # (channels, height, width)
+                 prev_channels: int = None,
+                 kernel_size: int = 3,
+                 stride: int = 1,
+                 padding = 'same',
+                 bias: bool = True,
+                 upsample=1,
+                 maxpool=1,
+                 actv_fn: callable = F.tanh,
+                 d_actv_fn: callable = None,
+                 gamma: float = 0.1,
+                 beta: float = 1.0,
+                 device=torch.device('cpu'),
+                 dtype=None
+                 ) -> None:
+        
+        super().__init__(shape, prev_channels, kernel_size, stride, padding, bias, upsample, maxpool, actv_fn, d_actv_fn, gamma, beta, device, dtype)
+
+
+    def init_weights(self):
+        # Initialise weights if not input layer
+        if self.prev_channels is not None:
+            self.conv_td = nn.Sequential(
+                nn.MaxPool2d(kernel_size=self.maxpool),
+                nn.Upsample(scale_factor=self.upsample),
+                nn.ConvTranspose2d(self.shape[0], self.prev_channels, self.kernel_size, stride=self.stride, padding=self.padding, bias=self.bias, **self.factory_kwargs),
+            )
+            # self.conv_bu = nn.Sequential(
+            #     nn.Upsample(scale_factor=maxpool),
+            #     nn.ConvTranspose2d(prev_shape[0], shape[0], kernel_size, padding=padding, stride=stride, bias=False, **factory_kwargs),
+            # )
+
+
+
+
+
+
+# Whittington & Bogacz 2017
+class Conv2dSkip(Conv2d):
 
     def __init__(self,
                  shape: (int, int, int), # (channels, height, width)
@@ -240,39 +192,8 @@ class Conv2dSkip(nn.Module):
                  ) -> None:
         
         assert shape[0] % 2 == 0, "Number of channels must be even."
-        # assert stride == 1, "Stride != 1 not yet supported."
 
-        factory_kwargs = {'device': device, 'dtype': dtype}
-        super(Conv2dSkip, self).__init__()
-
-        self.shape = shape
-        # self.prev_shape = prev_shape
-        self.actv_fn = actv_fn
-        self.gamma = gamma
-        self.beta = beta
-        self.device = device
-
-
-        # Default derivative of activation function
-        if d_actv_fn is not None:
-            self.d_actv_fn: callable = d_actv_fn
-        elif actv_fn == F.relu:
-            self.d_actv_fn: callable = lambda x: torch.sign(torch.relu(x))
-        elif actv_fn == F.sigmoid:
-            self.d_actv_fn: callable = lambda x: torch.sigmoid(x) * (1 - torch.sigmoid(x))
-        elif actv_fn == F.tanh:
-            self.d_actv_fn: callable = lambda x: 1 - torch.tanh(x).square()
-        
-        # Initialise weights if not input layer
-        if prev_channels is not None:
-            self.conv_td = nn.Sequential(
-                nn.Conv2d(shape[0], prev_channels, kernel_size, padding=padding, stride=stride, bias=bias, **factory_kwargs),
-                nn.MaxPool2d(kernel_size=maxpool),
-            )
-            # self.conv_bu = nn.Sequential(
-            #     nn.Upsample(scale_factor=maxpool),
-            #     nn.ConvTranspose2d(prev_shape[0], shape[0], kernel_size, padding=padding, stride=stride, bias=False, **factory_kwargs),
-            # )
+        super(Conv2d, self).__init__(shape, prev_channels, kernel_size, stride, padding, bias, maxpool, actv_fn, d_actv_fn, gamma, beta, device, dtype)
 
     def init_state(self, batch_size):
         return {
@@ -280,21 +201,11 @@ class Conv2dSkip(nn.Module):
             'e': torch.zeros((batch_size, self.shape[0], self.shape[1], self.shape[2]), device=self.device),
         }
 
-    def to(self, *args, **kwargs):
-        self.device = args[0]
-        return super().to(*args, **kwargs)
-
     # Returns a prediction of the state in the previous layer
     def predict(self, state, skip):
         x = torch.cat([state['x'], skip], dim=1)
-
         return self.conv_td(self.actv_fn(x))
-    
-    # propagates error from layer below, return an update for x
-    def propagate(self, e_below):
-        # return self.conv_bu(e_below)
-        raise(NotImplementedError("propagate not implemented for Conv2d"))
-    
+
     # Recalculates prediction-error between state and top-down prediction of it
     # With simulated annealing
     def update_e(self, state, skip, pred=None, temp=None):
@@ -305,29 +216,6 @@ class Conv2dSkip(nn.Module):
         if temp is not None:
             eps = torch.randn_like(state['e'], device=self.device) * 0.034 * temp
             state['e'] += eps
-
-    def update_x(self, state, e_below=None):
-        state['x'] = state['x'].detach() - self.gamma * state['x'].grad
-        state['x'].requires_grad = True
-
-    # def update_x(self, state, e_below=None):
-        # # If not input layer, propagate error from layer below
-        # if e_below is not None:
-        #     if e_below.dim() == 2:
-        #         e_below = e_below.unsqueeze(-1).unsqueeze(-1)
-        #     update = self.propagate(e_below)
-        #     state['x'] += self.gamma * (-state['e'] + update * self.d_actv_fn(state['x']))
-        # # This update will be zero if top layer
-        # state['x'] += self.gamma * (self.beta * -state['e'])
-        
-    # def update_grad(self, state, e_below):
-    #     if state['e'].norm() > 0:
-    #         state['e'].square().sum().backward()
-
-
-
-
-
 
 
 
